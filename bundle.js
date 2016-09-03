a52c16da2ee36d3510cb5ff71e8f739535b3f97d
Beau Cronin <beau.cronin@gmail.com>
2016-09-03 14:35:23 -0700

    new top-level functions in bundle

--- bundle.js
+++ bundle.js
@@ -352,11 +352,31 @@ var animateEmbeddings = function() {
 	}
 }
 
+function initScene() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 10;
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+	embedding.embed();
+    renderer.render( scene, camera );
+
+}
+
 module.exports = {
 	Dataset: Dataset,
 	Embedding: Embedding,
 	RandomEmbedding: RandomEmbedding,
-	ScatterEmbedding: ScatterEmbedding
+	ScatterEmbedding: ScatterEmbedding,
+	initScene: initScene,
+	animate: animate
 }
 
 'use strict';
