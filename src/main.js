c35324351508ebbc11baa943a39586f313d81c40
Beau Cronin <beau.cronin@gmail.com>
2016-12-28 13:59:21 -0800

    replace weber-viewer with webvr-boilerplate

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 'use strict'
 
-import OrbitControls from 'three-orbit-controls';
 import RayInput from 'ray-input';
 
 import queryString from 'query-string';
@@ -17,51 +16,64 @@ import {
 } from './embedding.js';
 import { detectMode } from './detection-utils.js';
 
-var controls;
-
-var embeddings = [];
-
-var animateEmbeddings = function() {
-	for (e in embeddings) {
-		//
-	}
-}
-
 export function initScene(controlType = "") {
-	if (controlType.toLowerCase() == "vr") {
-		// use mflux's webvr harness
-		const { scene, camera, renderer, events, toggleVR, controllers, vrEffect } = VRViewer({THREE});
-		return { scene, camera, renderer };
-	} else {
-		const scene = new THREE.Scene();
-		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-		camera.position.z = 10;
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 10;
+	const cameraControls = new THREE.VRControls(camera);
+	cameraControls.standing = true;
 
-		const renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-	    document.body.appendChild( renderer.domElement );
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setPixelRatio(window.devicePixelRatio);
+    document.body.appendChild( renderer.domElement );
+    const effect = new THREE.VREffect(renderer);
+	effect.setSize( window.innerWidth, window.innerHeight );
 
-	    if (controlType.toLowerCase() == "orbit") {
-	    	// const OrbitControls = require('three-orbit-controls')(THREE);
-	    	controls = new OrbitControls(camera, renderer.domElement);
-	    }
+	const manager = new WebVRManager(renderer, effect);
 
-	    // putting the input in the THREE global for now; probably want embeddings to fire 
-	    // events when meshes are added/removed rather than referencing the input directly
-		THREE.input = new RayInput(camera, renderer.domElement);
-		THREE.input.on('rayover', (mesh) => console.log(mesh));
-		THREE.input.setSize(renderer.getSize());
-		
-	    return { scene, camera, renderer };		
+	var onResize = function(e) {
+	  effect.setSize(window.innerWidth, window.innerHeight);
+	  camera.aspect = window.innerWidth / window.innerHeight;
+	  camera.updateProjectionMatrix();
 	}
+
+	window.addEventListener('resize', onResize, true);
+	window.addEventListener('vrdisplaypresentchange', onResize, true);
+
+    // putting the input in the THREE global for now; probably want embeddings to fire 
+    // events when meshes are added/removed rather than referencing the input directly
+	THREE.input = new RayInput(camera, renderer.domElement);
+	THREE.input.on('rayover', (mesh) => console.log('rayover '+mesh.userData.description));
+	THREE.input.on('raydown', (mesh) => console.log('RAYDOWN '+mesh.userData.description));
+	THREE.input.setSize(renderer.getSize());
+
+	// NOTE: relies on the polyfill to always have a valid display
+	var vrDisplay;
+	navigator.getVRDisplays().then(function(displays) {
+	    if (displays.length > 0) {
+	    	console.log(displays);
+	      vrDisplay = displays[0];
+	      vrDisplay.requestAnimationFrame(animate);
+	    }
+	});
+	console.log(vrDisplay);
+
+    return { scene, camera, manager, effect, cameraControls, vrDisplay };
 }
 
-export function animate() {
+var lastRender = 0;
+export function animate(timestamp) {
+	if (! timestamp) timestamp = Date.now();
+	var delta = Math.min(timestamp - lastRender, 500);
+  	lastRender = timestamp;
 
-    requestAnimationFrame( animate );
 	embedding.embed();
 	THREE.input.update();
-    renderer.render( scene, camera );
+    cameraControls.update();
+    manager.render( scene, camera, timestamp );
+    effect.render( scene, camera );
+    vrDisplay.requestAnimationFrame( animate );
 
 }
 
