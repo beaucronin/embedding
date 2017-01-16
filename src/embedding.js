f787fa0dec3a6795411dbb51d3bacb609692ebaa
Beau Cronin <beau.cronin@gmail.com>
2017-01-16 15:40:10 -0800

    add text align option to console embedding

--- src/embedding.js
+++ src/embedding.js
@@ -557,13 +557,14 @@ export class ConsoleEmbedding extends Embedding {
 	constructor(scene, dataset, options={}) {
 		options = assign({
 			font: "Bold 24px Arial",
-			fillStyle: "rgba(255,0,0,0.95)",
-			align: "center"
+			fillStyle: "rgba(225,225,225,0.90)",
+			align: "center",
+			width: 1.0
 		}, options);
 		super(scene, dataset, options);
 		this.canvas = document.createElement('canvas');
-		this.canvas.width = 256;
-		this.canvas.height = 128;
+		this.canvas.width = 1024;
+		this.canvas.height = 512;
 		this.context = this.canvas.getContext('2d');
 		this.context.font = this.getOpt('font');
 		this.context.fillStyle = this.getOpt('fillStyle');
@@ -574,15 +575,23 @@ export class ConsoleEmbedding extends Embedding {
 	setText(text) {
 		if (this.mesh)
 			this.obj3D.remove(this.mesh)
-
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		this.context.fillText(text, 0, 25);
+
+		if (this.getOpt('align') == 'left')
+			this.context.fillText(text, 0, this.canvas.height / 2);
+		else if (this.getOpt('align') == 'right')
+			this.context.fillText(text, this.canvas.width, this.canvas.height / 2);
+		else // default to center
+			this.context.fillText(text, this.canvas.width / 2, this.canvas.height / 2);
+
 		let texture = new THREE.Texture(this.canvas);
 		texture.needsUpdate = true;
 		let material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
 		material.transparent = true;
+
 		this.mesh = new THREE.Mesh(
-			new THREE.PlaneGeometry(this.canvas.width * .1, this.canvas.height * .1),
+			new THREE.PlaneGeometry(
+				this.getOpt('width') * this.canvas.width / 256, this.getOpt('width') * this.canvas.height / 128),
 			material
 		);
 		this.mesh.position.set(this.getOpt('x'), this.getOpt('y'), this.getOpt('z'));
